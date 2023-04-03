d1b835d8f447b2517cea0cc8b385fb3fc4f96876
Eddie <erg1101@outlook.com>
2023-04-02 21:28:58 -0400

    Resource packs are now downloaded as well

--- CFModDownloader.py
+++ CFModDownloader.py
@@ -18,8 +18,8 @@ def get_CF_slug(html):
     soup = BeautifulSoup(html, 'html.parser')
     for link in soup.find_all('a'):
         url = link.get('href')
-        if url.startswith('https://') and '/mc-mods/' in url:
-            return url.split('/')[-1]
+        if url.startswith('https://') and ('/mc-mods/' in url or '/texture-packs/' in url):
+            return (url.split('/')[-1], True if '/texture-packs/' in url else False)
 
 
 def main():
@@ -36,11 +36,13 @@ def main():
         html = get_CFL_HTML(projectID, fileID)
         slug = get_CF_slug(html)
         if slug is None:
-            print(f'Could not find mod (it\'s probably a resource pack). Try plugging these numbers into www.cflookup.com:\n\tProject ID: {projectID}\n\tFile ID: {fileID}')
+            print(f'Could not find mod. Try plugging these numbers into www.cflookup.com:\n\tProject ID: {projectID}\n\tFile ID: {fileID}')
         else:
-            download_link = f'https://www.curseforge.com/minecraft/mc-mods/{slug}/download/{fileID}'
-            print(f'Downloading {slug} ({download_link})...')
+            download_link = f'https://www.curseforge.com/minecraft/{"texture-packs" if slug[1] else "mc-mods"}/{slug[0]}/download/{fileID}'
+            print(f'Downloading {slug[0]} ({download_link})...')
             web.open(download_link)
+
 
 if __name__ == '__main__':
     main()
+
